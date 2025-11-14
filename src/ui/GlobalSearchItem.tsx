47b52c208d547e8b94706eb8fe991c5088130d75
Samuel <lakamosamuel7@gmail.com>
2025-11-15 00:34:24 +0100

    Fixed: edit the input drop down

--- src/ui/GlobalSearchItem.tsx
+++ src/ui/GlobalSearchItem.tsx
@@ -11,14 +11,14 @@ const GlobalSearchItem: React.FC<ItemProps> = ({ item, isActive, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className={`p-3 rounded-xl cursor-pointer border dark:border-white/10
-      ${isActive ? "bg-white/20 dark:bg-white/10" : "hover:bg-white/10"}
+      className={`p-3 rounded-xl cursor-pointer border  border-gray-300 dark:border-white/10
+      ${isActive ? "bg-white/10 border-blue-600 dark:bg-gray-900" : "hover:bg-gray/50"}
       transition`}
     >
       <p className="text-xs text-gray-400 mb-1">{item.type}</p>
 
       <p
-        className="text-sm font-medium"
+        className="text-sm font-medium dark:text-white"
         dangerouslySetInnerHTML={{ __html: item.highlighted }}
       />
     </div>
